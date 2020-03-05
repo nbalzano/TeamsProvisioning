d5102a9b45f3b3d5533602eaf41db0a9db270c2b
nbalzano <nicola.balzano@live.com>
2020-03-05 08:26:19 +0100

    Update InstallDev.ps1

--- TeamsProvisioningRG/InstallDev.ps1
+++ TeamsProvisioningRG/InstallDev.ps1
@@ -1,11 +1,11 @@
 # Provision storage and function app
 .\Deploy-AzureResourceGroup.ps1 `
-    -ResourceGroupLocation EastUS `
-    -ResourceGroupName TeamsProvisioningDev `
+    -ResourceGroupLocation West Europe `
+    -ResourceGroupName TeamCreatorRG `
     -TemplateParametersFile dev.parameters.json
 
 # Ensure storage queues are in place
-$storageAccount = Get-AzureRmStorageAccount | Where-Object ResourceGroupName -eq "TeamsProvisioningDev"
+$storageAccount = Get-AzureRmStorageAccount | Where-Object ResourceGroupName -eq "TeamCreatorRG"
 $storageContext = $storageAccount.Context
 New-AzureStorageQueue -Name "create-team-request-queue" -Context $storageContext -ErrorAction Ignore
 New-AzureStorageQueue -Name "create-team-completion-queue" -Context $storageContext -ErrorAction Ignore
